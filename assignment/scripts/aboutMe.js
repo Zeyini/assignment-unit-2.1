de7cac1e22690ab4b0f1e9db70b55684eb676bbd
Zeyini Mohammad <Zeyinim@outlook.com>
2024-02-28 23:23:03 -0600

    created a function to test if adventurous is true or falseand assigned result variable a value within the function

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -40,12 +40,17 @@ let friendsPets = 1;
 pets += 2;
 console.log(pets);
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets =  3;
 // 12 - Create a variable called `result`. Create a conditional:
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
-
+let result = " ";
+if (adventurous = true) {
+  console.log("Adventures are great!")
+} else {
+  console.log("How about we stay home")
+}
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
 //      if luckyNumber is 2 and adventurous is true,
